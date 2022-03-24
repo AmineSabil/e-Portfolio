6a71baa4b7b99547c94d83f728dec45551f21fb0
Name <user@example.com>
2022-03-24 13:04:12 +0100

    finish intro

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,10 +1,38 @@
 import "./intro.scss"
-
+import SentimentVerySatisfied from '@material-ui/icons/SentimentVerySatisfied';
+import { init } from 'ityped';
+import { useEffect,useRef } from "react";
 
 export default function Intro() {
+    
+    const textRef = useRef()
+    useEffect(() => {
+        init(textRef.current, {
+            showCursor: true,
+            backDelay: 1500,
+            backSpeed:60,
+            strings: ["Web Developer", "AI developer", "Game developer"],
+        });
+    },[])
+
     return (
         <div className="intro" id="intro">
-           
+           <div class="left">
+              <div className="imgContainer">
+                 <img src="assets/profile_picture.png" alt="profile"/>
+              </div>
+           </div>
+           <div class="right">
+                <div className="wrapper">
+                    <h2>  Hi there <SentimentVerySatisfied/>, I'm</h2>
+                    <h1>Mohamed Amine SABIL</h1>
+                    <h3>Freelance <span ref={textRef}></span></h3>
+                    <h3>Computer Engineering <span>Student</span></h3> 
+                </div>
+                <a href="#portfolio">
+                    <img src="assets/down.png" alt=""/>
+                </a>
+           </div>
         </div>
     )
 }
--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,12 +1,12 @@
 import "./topbar.scss"
-import {Email,Person} from '@material-ui/icons';
+import {Email,Person,GitHub} from '@material-ui/icons';
 
 export default function Topbar({menuOpen,setMenuOpen}) {
     return (
         <div className={"topbar " + (menuOpen && "active")}  >
             <div className="wrapper">
                  <div className="left">
-                     <a href="#intro" className="logo"> M. Amine SABIL </a>
+                     <a href="#intro" className="logo"> ven2ju </a>
                      <div className="itemContainer">
                         <Person className="icon"/>
                         <span>+33 7 53 26 09 98</span>
@@ -15,8 +15,11 @@ export default function Topbar({menuOpen,setMenuOpen}) {
                         <Email className="icon"/>
                         <span>aminesabil2000@gmail.com</span>
                      </div>
+                     <div className="itemContainer">
+                        <GitHub className="icon"/>
+                        <span>AmineSabil</span>
+                     </div>
                  </div>
-                 <h1>Hello</h1>
                  <div className="right">
                      <div className="hamburger" onClick={ () => setMenuOpen(!menuOpen)}>
                          <span className="line1"> </span>
